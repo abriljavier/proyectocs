4e45cf6967c6a8f304381a7d7bdbcbb2e00fdf7e
misterjoepink <abrilgranelljavier@gmail.com>
2024-02-04 17:00:54 +0100

    fix(headers): headers no longer have own <head>

--- js/header.js
+++ js/header.js
@@ -1,29 +1,31 @@
 $(document).ready(function () {
+    setTimeout(function () {
+        //EXTRAER EN QUE PÁGINA ESTOY
+        var url = window.location.href;
+        var fileNameWithExtension = url.substring(url.lastIndexOf('/') + 1);
+        var fileNameWithoutExtension = fileNameWithExtension.substring(0, fileNameWithExtension.lastIndexOf('.'));
+        switch (fileNameWithoutExtension) {
+            case "login":
+                $('button#private').text("Página privada");
+                $('button#private').on("click", function () {
+                    location.href = "login.html";
+                })
+                break;
+            case "private":
+                $('button#private').text("Cerrar sesión");
+                break;
+            default:
+                $('button#private').text("Página privada");
+                $('button#private').on("click", function () {
+                    location.href = "login.html";
+                })
+                break;
+        }
 
-    //EXTRAER EN QUE PÁGINA ESTOY
-    var url = window.location.href;
-    var fileNameWithExtension = url.substring(url.lastIndexOf('/') + 1);
-    var fileNameWithoutExtension = fileNameWithExtension.substring(0, fileNameWithExtension.lastIndexOf('.'));
-    switch (fileNameWithoutExtension) {
-        case "login":
-            $('button#private').text("Página privada");
-            $('button#private').on("click", function () {
-                location.href = "login.html";
-            })
-            break;
-        case "private":
-            $('button#private').text("Cerrar sesión");
-            break;
-        default:
-            $('button#private').text("Página privada");
-            $('button#private').on("click", function () {
-                location.href = "login.html";
-            })
-            break;
-    }
 
+        $('button#cart').on("click", function () {
+            location.href = "cart.html";
+        })
+    }, 500);
 
-    $('button#cart').on("click", function () {
-        location.href = "cart.html";
-    })
 });
